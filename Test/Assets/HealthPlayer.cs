91769b647a368a86ce228baa0c608019190a5ec4
RedMark99 <RedMarkGame@gmail.com>
2020-06-23 15:24:57 +0300

    23.06.2020
    23.06.2020

---
+++ Test/Assets/HealthPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPlayer : MonoBehaviour
+{
+    public int health = 100;
+    public Slider slider;
+    public Text textSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = health;   
+        textSlider.text = health.ToString() + "/100 HP";
+    }
+}
---
+++ Test/Assets/NPC_Task.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Task : MonoBehaviour {
+	public bool EndDialog;
+	public GameObject Dialog1;
+	public GameObject Dialog2;
+	public bool check;
+	public Quest_Event QE;
+	public bool fin_Dialog;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		if (EndDialog == true) {
+			Time.timeScale = 1;
+			QE.Quest1 = true;
+			Dialog1.SetActive (false);
+		}
+		if(fin_Dialog == true)
+		{
+			Time.timeScale = 1;
+			QE.Quest1 = false;
+			check = true;
+			Dialog2.SetActive(false);
+		}
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") {
+			Time.timeScale = 0;
+			if(QE.end_Quest1 == false)
+			{
+				Dialog1.SetActive (true);
+			}
+			else if (QE.end_Quest1 == true && check == false)
+			{
+				Dialog2.SetActive (true);
+			}
+		}
+	}
+}
